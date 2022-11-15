307e7bfa07c247b6c02f282dde2d9f6c448101d5
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-15 15:53:23 +0600

    first commit

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomething() {
+    console.log('I am coding JavaScript');
+}
+console.log('first: ami sobar age ');
+console.log('second: I am the second person');
+// setTimeout(doSomething, 3000);
+setTimeout(function () {
+    console.log('I am using coding vs code')
+}, 5000)
+setTimeout(() => {
+    console.log('Exploring MDN articles');
+}, 4000)
+console.log('Third: ami 3 number baccha');
+console.log('Fourht: am ied hali purno korsi'); 
